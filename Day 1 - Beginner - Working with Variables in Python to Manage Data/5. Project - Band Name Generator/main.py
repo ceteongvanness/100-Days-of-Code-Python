de6f8ca125fb813fe9f7edb30dc944fa56850cd0
Eng Teong <ceteongvanness@hotmail.com>
2020-11-12 22:15:54 +0800

    Project: Band Name Generator

---
+++ Day 1 - Beginner - Working with Variables in Python to Manage Data/5. Project - Band Name Generator/main.py
@@ -0,0 +1,8 @@
+#1. Create a greeting for your program.
+print("Welcome to the Band Name Generator.")
+#2. Ask the user for the city that they grew up in.
+street = input("What's name of the city you grew up in?\n")
+#3. Ask the user for the name of a pet.
+pet = input("What's your pet's name? \n")
+#4. Combine the name of their city and pet and show them their band name.
+print("Your band name could be " + street + " " + pet)
