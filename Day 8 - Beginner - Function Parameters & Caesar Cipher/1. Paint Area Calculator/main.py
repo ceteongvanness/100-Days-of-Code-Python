183f8da602ad79523c37fa9055d9bb6dba7df683
Eng Teong <ceteongvanness@hotmail.com>
2020-11-19 23:01:40 +0800

    Day 8 - Beginner - Function Parameter & Caesar Cipher

---
+++ Day 8 - Beginner - Function Parameters & Caesar Cipher/1. Paint Area Calculator/main.py
@@ -0,0 +1,11 @@
+import math
+
+def paint_calc(height, width, cover):
+    num_cans = (height * width) / cover
+    round_up_cans = math.ceil(num_cans)
+    print(f"You'll need {round_up_cans} cans of paint.")
+
+test_h = int(input("Height of wall: "))
+test_w = int(input("Width of wall: "))
+coverage = 5
+paint_calc(height=test_h, width=test_w, cover=coverage)
---
+++ Day 8 - Beginner - Function Parameters & Caesar Cipher/2. Prime Number Checker/main.py
@@ -0,0 +1,12 @@
+def prime_checker(number):
+    is_prime = True
+    for i in range(2, number):
+        if number % i == 0:
+            is_prime = False
+    if is_prime:
+        print("It's a prime number.")
+    else:
+        print("It's not a prime number.")
+
+n = int(input("Check this number: "))
+prime_checker(number=n)
