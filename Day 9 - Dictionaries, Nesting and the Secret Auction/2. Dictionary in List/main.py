5bca4fa53eaf0909dd293fd39f8b6e3324c61fd8
Eng Teong <ceteongvanness@hotmail.com>
2020-11-21 09:24:44 +0800

    Day 9 - Dictionaries, Nesting and the Secret Auction

---
+++ Day 9 - Dictionaries, Nesting and the Secret Auction/2. Dictionary in List/main.py
@@ -0,0 +1,26 @@
+travel_log = [
+{
+  "country": "France",
+  "visits": 12,
+  "cities": ["Paris", "Lille", "Dijon"]
+},
+{
+  "country": "Germany",
+  "visits": 5,
+  "cities": ["Berlin", "Hamburg", "Stuttgart"]
+},
+]
+#DO NOT change the code above 👆
+
+#TODO: Write the function that will allow new countries
+#to be added to the travel_log.
+def add_new_country(name, visit_count, cities_visited):
+  new_country = {}
+  new_country["country"] = name
+  new_country["visits"] = visit_count
+  new_country["cities"] = cities_visited
+  travel_log.append(new_country)
+
+#Do not change the code below 👇
+add_new_country("Russia", 2, ["Moscow", "Saint Petersburg"])
+print(travel_log)
