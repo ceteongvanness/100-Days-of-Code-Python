0c5987bd2be8720adac76485b621e1ac1ac73cd0
Cheah Eng Teong <ceteongvanness@hotmail.com>
2020-12-13 21:30:48 +0800

    Day 30 - Errors, Exceptions and JSON Data

--- Day 30 - Intermediate - Errors, Exceptions and JSON Data - Improving the Password/main.py
+++ Day 30 - Intermediate - Errors, Exceptions and JSON Data - Improving the Password/main.py
@@ -100,7 +100,7 @@ website_entry.grid(row=1, column=1)
 website_entry.focus()
 email_entry = Entry(width=35)
 email_entry.grid(row=2, column=1, columnspan=2)
-email_entry.insert(0, "angela@gmail.com")
+email_entry.insert(0, "etcheah@gmail.com")
 password_entry = Entry(width=21)
 password_entry.grid(row=3, column=1)
 
