25d46536418324eb034a07d9014a044f709e2436
Cheah Eng Teong <ceteongvanness@hotmail.com>
2020-12-28 12:03:10 +0800

    Day 47  - Create an Automated Price Tracker

---
+++ Day 47 - Intermediate - Create an Automated Amazon Price Tracker/1. Project - Automated Amazon Price Tracker/main.py
@@ -0,0 +1,18 @@
+import smtplib
+
+title = soup.find(id="productTitle").get_text().strip()
+print(title)
+
+BUY_PRICE = 200
+
+if price_as_float < BUY_PRICE:
+    message = f"{title} is now {price}"
+
+    with smtplib.SMTP(YOUR_SMTP_ADDRESS, port=587) as connection:
+        connection.starttls()
+        result = connection.login(YOUR_EMAIL, YOUR_PASSWORD)
+        connection.sendmail(
+            from_addr=YOUR_EMAIL,
+            to_addrs=YOUR_EMAIL,
+            msg=f"Subject:Amazon Price Alert!\n\n{message}\n{url}"
+        )
